b5e371d3c1a37d7d48672fa1c19ef122321500d8
ADISH.A.P <helloadish007@gmail.com>
2022-08-25 09:07:22 +0530

    Update prod_v1.py

--- prod_v1.py
+++ prod_v1.py
@@ -54,7 +54,8 @@ with st.sidebar.expander("Demo File"):
         file_name='demo_df.csv',
         mime='text/csv',
     )
-    st.sidebar.image("https://the-dots.com/pages/google-brand-studio-134761")
+    st.sidebar.image("https://images1.the-dots.com/2440667/the-dots-background.png?p=cover")
+    
 
 uploaded_file = st.file_uploader("Upload File : ", type={"csv", "txt"})
 if uploaded_file is not None:
